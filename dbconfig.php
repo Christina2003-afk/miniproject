4afabf55ffc1bbc5826725805660409a1376a960
christina babu <christinababu03@gmail.com>
2025-04-08 10:26:18 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-// PostgreSQL connection settings from Render
+// PostgreSQL connection info from Render
 $host = "dpg-cvqa16uuk2gs73d0m94g-a.oregon-postgres.render.com";
 $port = "5432";
 $dbname = "dbart";
@@ -9,13 +9,13 @@ $password = "sT0kRz65o76nqAlYUc4gCh4mFZK2B1GZ";
 // Connection string
 $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
-// Attempt to connect
+// Connect to PostgreSQL
 $conn = pg_connect($conn_string);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 } else {
-    // echo "Connected to PostgreSQL database successfully!";
+    // echo "Connected to PostgreSQL!";
 }
 ?>
